b5099c944cfb3f0c0e57f4d39d76e5c38ab51fb3
helloworld2523 <helloworld2523@gmail.com>
2017-11-23 12:49:09 +0700

    get error

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ if (!is_null($events['events'])) {
                     $respMessage = 'Hello, your message is '. $event['message']['text'];
             
                     $httpClient = new CurlHTTPClient($channel_token);
+                    error_log( $httpClient );
                     $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
         
                     $textMessageBuilder = new TextMessageBuilder($respMessage);
