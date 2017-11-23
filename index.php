17deaa2a861e12b1aaad9db0276d738c29138360
helloworld2523 <helloworld2523@gmail.com>
2017-11-23 15:08:32 +0700

    comment

--- index.php
+++ index.php
@@ -21,7 +21,11 @@
  */
 
 //include ('vendor/autoload.php');
-require_once ('app/vendor/autoload.php');
+// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once ('../vendor/autoload.php');
 //$path = __DIR__ . '/vendor/autoload.php';
 //error_log($path);
 //require_once($path);
@@ -36,40 +40,43 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'yjBtwbJsQU9xYHkUIDswtYkn4z4hC0eYbWYvTY4pYrABSNAHwwLXOLh61iF64WA5liHg1hzrd0HnC5ya3cLCOISlJZEG1BjGy0mDJ/XtOgtCKeRPzHSY3BTQmyJw5ByxTRQieEwSZoQWXURGQgodGwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'a6828dff8c16162227198d1ef3467c68';
 
-// Get message from Line API
-$content = file_get_contents('php://input');
-error_log( $content );
-$events = json_decode($content, true);
-//error_log($events['events']);
-if (!is_null($events['events'])) {
+// เชื่อมต่อกับ LINE Messaging API
+$httpClient = new CurlHTTPClient($channel_token);
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+// // Get message from Line API
+// $content = file_get_contents('php://input');
+// error_log( $content );
+// $events = json_decode($content, true);
+// //error_log($events['events']);
+// if (!is_null($events['events'])) {
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-        error_log($event['type']);
-        // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-            error_log($event['message']['type']);
-            switch($event['message']['type']) {
+// 	// Loop through each event
+// 	foreach ($events['events'] as $event) {
+//         error_log($event['type']);
+//         // Line API send a lot of event type, we interested in message only.
+// 		if ($event['type'] == 'message') {
+//             error_log($event['message']['type']);
+//             switch($event['message']['type']) {
                 
-                case 'text':
-                    // Get replyToken
-                    $replyToken = $event['replyToken'];
-                    error_log($replyToken);
-                    // Reply message
-                    $respMessage = 'Hello, your message is '. $event['message']['text'];
-                    error_log($channel_token);
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    error_log( $httpClient );
-                    //$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                    $bot = new LINEBot($httpClient,array('channelSecret' => $channel_secret));
+//                 case 'text':
+//                     // Get replyToken
+//                     $replyToken = $event['replyToken'];
+//                     error_log($replyToken);
+//                     // Reply message
+//                     $respMessage = 'Hello, your message is '. $event['message']['text'];
+//                     error_log($channel_token);
+//                     $httpClient = new CurlHTTPClient($channel_token);
+//                     error_log( $httpClient );
+//                     //$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+//                     $bot = new LINEBot($httpClient,array('channelSecret' => $channel_secret));
         
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+//                     $textMessageBuilder = new TextMessageBuilder($respMessage);
+//                     $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                     
-                    break;
-            }
-		}
-	}
-}
+//                     break;
+//             }
+// 		}
+// 	}
+// }
 
 echo "OK";
