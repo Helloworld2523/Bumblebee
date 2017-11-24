4c28b07895bd7866d9b05848e303b37f40c188c0
helloworld2523 <helloworld2523@gmail.com>
2017-11-24 11:45:02 +0700

    ----

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 require_once('./vendor/autoload.php');
-// // Namespace
-// use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-// use \LINE\LINEBot;
-// use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-// $channel_token =
-// 'yjBtwbJsQU9xYHkUIDswtYkn4z4hC0eYbWYvTY4pYrABSNAHwwLXOLh61iF64WA5liHg1hzrd0HnC5ya3cLCOISlJZEG1BjGy0mDJ/XtOgtCKeRPzHSY3BTQmyJw5ByxTRQieEwSZoQWXURGQgodGwdB04t89/1O/w1cDnyilFU=';
-// $channel_secret = 'a6828dff8c16162227198d1ef3467c68';
-// // Get message from Line API
-// $content = file_get_contents('php://input');
-// $events = json_decode($content, true);
-echo $test;
+// Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token =
+'yjBtwbJsQU9xYHkUIDswtYkn4z4hC0eYbWYvTY4pYrABSNAHwwLXOLh61iF64WA5liHg1hzrd0HnC5ya3cLCOISlJZEG1BjGy0mDJ/XtOgtCKeRPzHSY3BTQmyJw5ByxTRQieEwSZoQWXURGQgodGwdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'a6828dff8c16162227198d1ef3467c68';
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
 echo 'OK';
