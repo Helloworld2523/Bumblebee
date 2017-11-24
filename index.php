c40093a4711b247b9d67b69e0adb43d8007ff544
Songwut Chaisamut <da@Songwuts-MacBook-Pro.local>
2017-11-24 22:11:11 +0700

    case image

--- index.php
+++ index.php
@@ -19,49 +19,43 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
-	
-	// 	Loop through each event
-	foreach ($events['events'] as $event) {
-		
-		// 		Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-			
-			switch($event['message']['type']) {
-				
-				case 'text':
-				// 				Get replyToken
-				$replyToken = $event['replyToken'];
-				
-				// 				Reply message
-				$respMessage = 'Hello, your message is '. $event['message']['text'];
-				
-				$httpClient = new CurlHTTPClient($channel_token);
-				
-				$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-				
-				$textMessageBuilder = new TextMessageBuilder($respMessage);
-				
-				$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-				
-				break;
-				case 'image': 
-					$messageID = $event['message']['id']; 
-					$respMessage = 'Hello, your image ID is '. $messageID; 
-					$httpClient = new CurlHTTPClient($channel_token);
-					
-					$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-					
-					$textMessageBuilder = new TextMessageBuilder($respMessage);
-					
-					$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-				break;
-				
-			}
-			
-		}
-		
-	}
-	
+    // 	Loop through each event
+    foreach ($events['events'] as $event) {
+        // 		Line API send a lot of event type, we interested in message only.
+        if ($event['type'] == 'message') {
+            switch ($event['message']['type']) {
+                case 'text':
+                // 				Get replyToken
+                    $replyToken = $event['replyToken'];
+                
+                // 				Reply message
+                    $respMessage = 'Hello, your message is '. $event['message']['text'];
+                
+                    $httpClient = new CurlHTTPClient($channel_token);
+                
+                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                
+                    break;
+                case 'image':
+                    $messageID = $event['message']['id'];
+                
+                    // Create image on server.
+                    $fileID = $event['message']['id'];
+                    $response = $bot->getMessageContent($fileID);
+                    $fileName = 'linebot.jpg';
+                    $file = fopen($fileName, 'w');
+                    fwrite($file, $response->getRawBody());
+                
+                     // Reply message
+                    $respMessage = 'Hello, your image ID is '. $messageID;
+                    break;
+            }
+        }
+    }
 }
 
 
